114cd0adb1da936173d64728a53b04800cbedcee
Alexander Holas <alex.holas@gmx.de>
2022-06-10 15:22:01 +0200

    Added environment file

---
+++ sccala/sccala_scm.py
@@ -0,0 +1,32 @@
+import os
+import re
+import argparse
+
+import numpy as np
+import pandas as pd
+
+import libio.get_paths as pa
+
+
+def main(args):
+
+    return
+
+
+def cli():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+    )
+    parser.add_argument(
+    )
+
+    args = parser.parse_args()
+
+    main(args)
+
+    return
+
+
+if __name__ == "__main__":
+    cli()
