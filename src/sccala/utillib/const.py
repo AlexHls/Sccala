fdb5b664c7e8fe84dd16b3f961f650352ee9cb69
Alexander Holas <alex.holas@gmx.de>
2025-03-21 14:41:01 +0100

    Fix units

--- src/sccala/utillib/const.py
+++ src/sccala/utillib/const.py
@@ -1,4 +1,4 @@
 C_LIGHT = 299792.458  # c in km/s
 H_ERG = 6.62607015e-27  # Plancks constant (erg.s)
 C_AA = 299792458 * 1.0e10  # in AA/s
-PV_UNCERTAINTY = 150.0  # Peculiar velocity uncertainty in m/s
+PV_UNCERTAINTY = 150.0  # Peculiar velocity uncertainty in km/s
